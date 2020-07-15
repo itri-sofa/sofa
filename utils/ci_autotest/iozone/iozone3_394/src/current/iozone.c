aed8f8e4f16c58450e8dfc9f84cb29e9a65f0df7
root <root@localhost.localdomain>
2020-07-15 14:55:22 +0800

    Fix #5 , Fix #4 and Fix #8

--- utils/ci_autotest/iozone/iozone3_394/src/current/iozone.c
+++ utils/ci_autotest/iozone/iozone3_394/src/current/iozone.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2025 Industrial Technology Research Institute.
+ * Copyright (c) 2015-2020 Industrial Technology Research Institute.
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -9,7 +9,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -17,6 +17,13 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ *
+ *
+ *
+ *
+ *
+ *
+ *  
  */
 /************************************************************************/
 /*	Original Author: 						*/
